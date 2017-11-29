9573a8623d0bc1a49a58bf4d4890dcef4708c375
Sadat Islam <sadat.islam@cs.ucalgary.ca>
2017-11-29 15:25:51 -0700

    Added all entity class files

---
+++ BanglaBazarFoodCourt/Models/Entities/Food_Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanglaBazarFoodCourt.Models.Entities
+{
+    public class Food_Item
+    {
+    }
+}
